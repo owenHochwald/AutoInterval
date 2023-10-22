1f23034c78560af1239c22df5bca4ed935619094
Owen Hochwald <oh@insomniacdesign.com>
2023-10-22 14:15:39 -0400

    Cleaned up code

--- main.py
+++ main.py
@@ -33,45 +33,43 @@ NUM_PITCHES = 12
 
 
 def get_starting_pitch() -> tuple:
+    print("-------------------")
     pitch = input("Enter a pitch: ")
-    for group_index, group in enumerate(pitches):
-        if pitch in group:
-            return pitch, group_index
-    else:
+    group_index = next((index for index, group in enumerate(pitches) if pitch in group), None)
+    while group_index is None:
         print("Invalid pitch. Please enter a pitch from the list.")
-        return get_starting_pitch()
+        pitch = input("Enter a pitch: ")
+        group_index = next((index for index, group in enumerate(pitches) if pitch in group), None)
+    return pitch, group_index
 
 
 def get_interval_distance() -> str:
-    interval = input("Enter an interval (P1, d2, m2, M2, d3, m3, M3, d4, P4, A4, d5, P5, d6, m6, M6, d7, m7, M7, d8): ")
-    if interval not in intervals:
+    while True:
+        interval = input("Enter an interval (P1, d2, m2, M2, d3, m3, M3, d4, P4, A4, d5, P5, d6, m6, M6, d7, m7, M7, d8): ")
+        if interval in intervals:
+            return interval
         print("Invalid interval. Please enter an interval from the list.")
-        return get_interval_distance()
-    return interval
+
 
 
 def get_direction() -> str:
-    direction = input("Enter direction (up or down): ")
-    if direction.lower() not in ["up", "down"]:
+    while True:
+        direction = input("Enter direction (up or down): ")
+        if direction.lower() in ["up", "down"]:
+            return direction
         print("Invalid direction. Please enter 'up' or 'down'.")
-        return get_direction()
-    return direction
 
 
 def calculate_interval(starting_pitch, pitch_index, interval_distance, direction):
-    # starting_pitch_group_index = pitches.index(starting_pitch)
     interval_distance_increment = intervals.get(interval_distance)
-
-    if direction == "up":
-        second_pitch_group_index = (pitch_index + interval_distance_increment) % NUM_PITCHES
-    else:
-        second_pitch_group_index = (pitch_index - interval_distance_increment) % NUM_PITCHES
-
+    second_pitch_group_index = (pitch_index + interval_distance_increment) % NUM_PITCHES if direction == "up" else (pitch_index - interval_distance_increment) % NUM_PITCHES
     second_pitch = pitches[second_pitch_group_index]
+
     if len(second_pitch) > 0:
         return f"Starting on {starting_pitch} and going {direction} a {interval_distance} gives you {second_pitch[0]}, which is a change of {interval_distance_increment} half-step/s."
     else:
         return f"Starting on {starting_pitch} and going {direction} a {interval_distance} gives you {second_pitch[0]} / {second_pitch[1]}, which is a change of {interval_distance_increment} half-step/s."
+
 
 if __name__ == '__main__':
     starting_pitch, pitch_index = get_starting_pitch()
